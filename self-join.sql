1aed3f43fe4105883208afb18fd282a520d5ef9e
Jervis <jerv0003@e.ntu.edu.sg>
2020-02-23 21:08:22 +0800

    Update self-join.sql

--- self-join.sql
+++ self-join.sql
@@ -88,5 +88,5 @@ JOIN (route c JOIN route d ON (c.company = d.company AND c.num = d.num))
 JOIN stops stopa ON (a.stop = stopa.id)
 JOIN stops stopb ON (b.stop = stopb.id)
 JOIN stops stopc ON (c.stop = stopc.id)
-JOIN stops stopd ON (d.stop = stopd.id)
+JOIN stops stopd ON (d.stop = stopd.id) 
 WHERE stopa.name = 'Craiglockhart' AND stopd.name = 'Lochend' AND stopb.name = stopc.name
